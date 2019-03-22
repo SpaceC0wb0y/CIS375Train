efdb1ec208bfb4557756fe04695b08330b5a5343
Ryan Klaus <Ryan_Klaus729@yahoo.com>
2019-03-21 23:07:20 -0400

    First Commit
    Fingers Crossed I'm doing this right

---
+++ Collision Detection/CollisionDetection/CollisionDetection/Collision.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace CollisionDetection
+{
+     class Collision
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("This will be my Collision Detection Program");
+          }
+     }
+}
---
+++ Rerouting/Rerouting/Rerouting/Rerouter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Rerouting
+{
+     class Rerouter
+     {
+          static void Main(string[] args)
+          {
+               Console.WriteLine("This will be my Rerouting Program");
+          }
+     }
+}
