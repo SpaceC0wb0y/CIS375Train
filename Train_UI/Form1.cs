a3f82a1113d3c1c64611b19b36922a60fe9a474a
John MacFadyen <spacecowboy2397@gmail.com>
2019-03-24 21:07:38 -0400

    Add files via upload
    Now Featuring:
    -Database Connection
    -Metrics form with labels
    -Can read a file for input
    
    Features in next release:
    -Info fetched from database to appear in form
    -Other things...

---
+++ Train_UI/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace Train_UI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Console.WriteLine("Getting Connection ...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection ...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+
+            Console.Read();
+            InitializeComponent();
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 secondForm = new Form2();
+            secondForm.Show();
+
+        }
+    }
+}
