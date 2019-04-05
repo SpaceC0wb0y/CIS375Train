17aa42f17d6e8bc515b107685bb5285ea569f325
maitrapatel <maitrabp@umich.edu>
2019-04-05 15:11:25 -0400

    Patch 3
    Calculated on time train percentage metric

--- RSdbconnection/RSdbconnection/test.cs
+++ RSdbconnection/RSdbconnection/test.cs
@@ -10,21 +10,32 @@ namespace ConnectMySQL
 {
     class Program
     {
+        public static double percentOnTime(ref List<List<string>> x, ref List<List<string>> y)
+        {
+               if (y.Count != 0)
+                    return ((x.Count / y.Count) * 100);
+               else
+                    Console.WriteLine("Cannot compute percent on time trains, 0 routes were executed!");
+                    return 0.0;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Getting Connection ...");
             DBConnect connection = new DBConnect();
             List<List<string>> routesExecutedMETRIC = connection.Select("select route_id, train_id, computed_Route, route_type from RSdatabase.inprog_routes where time_completed != NULL; ");
             List<List<string>> onTimeTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 0;");
+            double perOnTimeTrain = percentOnTime(ref onTimeTrainMETRIC, ref routesExecutedMETRIC);
+            Console.WriteLine(perOnTimeTrain);
             List<List<string>> lateTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 1;");
             List<List<string>> mostUsedTrainMETRIC = connection.Select("select train_id, num_times_used from RSdatabase.train order by num_times_used desc; ");
             List<List<string>> mostUsedHubMETRIC = connection.Select("select hub_id, num_times_used from RSdatabase.hub order by num_times_used desc; ");
             List<List<string>> mostUsedStationMETRIC = connection.Select("select station_id, num_times_used from RSdatabase.station order by num_times_used desc; ");
             List<List<string>> mostUsedTrackMETRIC = connection.Select("select track_id, num_times_used from RSdatabase.tracks order by num_times_used desc; ");
             List<List<string>> mostCollisionProneTrack = connection.Select("select track_id, detected_coll from RSdatabase.tracks order by detected_coll desc; ");
-
+          
                //If you want to know how to iterate through List of Lists, check Handy Resources in DesignDocs of Omar&Maitra
         }
+
     }
 
 }
