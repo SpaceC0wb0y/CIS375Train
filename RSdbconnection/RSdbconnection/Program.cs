0017467040e7c5b0973fdb5494c5ee67031c6eae
maitrapatel <maitrabp@umich.edu>
2019-04-22 21:03:49 -0400

    Patch5

--- RSdbconnection/RSdbconnection/Program.cs
+++ RSdbconnection/RSdbconnection/Program.cs
@@ -21,6 +21,26 @@ namespace RSdbconnection
           public DBConnect()
           {
                Initialize();
+               if(OpenConnection())
+               {
+                    Console.WriteLine("Connection to DB successful...");
+               }
+               else
+               {
+                    Console.WriteLine("Something went wrong while connecting to DB..");
+               }
+                    
+          }
+          ~DBConnect()
+          {
+               if(CloseConnection())
+               {
+                    Console.WriteLine("Connection is successfully closed..");
+               }
+               else
+               {
+                    Console.WriteLine("Something went wrong whie closing DB connection..");
+               }
           }
 
           //Initialize values
@@ -93,39 +113,25 @@ namespace RSdbconnection
                DBvalues = "'" + DBvalues.Replace(",", "','") + "'";
                string query = "Insert INTO " + tableName + "(" + DBvariables + ")" + " VALUES(" + DBvalues + ");";
                Console.Write(query);
-               //open connection
-               if (this.OpenConnection() == true)
-               {
-                    //create command and assign the query and connection from the constructor
-                    MySqlCommand cmd = new MySqlCommand(query, connection);
 
-                    //Execute command
-                    cmd.ExecuteNonQuery();
+               //create command and assign the query and connection from the constructor
+               MySqlCommand cmd = new MySqlCommand(query, connection);
 
-                    //close connection
-                    this.CloseConnection();
-               }
+               //Execute command
+               cmd.ExecuteNonQuery();
           }
 
           //Pre: Must pass a delete query through this function with appropriate syntax based on mySQL
           //Post: deletes the record of a given condition specified in query
           public void Delete(string query)
            {
-               if (this.OpenConnection() == true)
-               {
                     MySqlCommand cmd = new MySqlCommand(query, connection);
                     cmd.ExecuteNonQuery();
-                    this.CloseConnection();
-               }
            }
           //Pre: Must pass an update query through this function with appropriate syntax based on mySQL
           //Post: Updates the record of a given condition specified in query
           public void Update(string query)
           { 
-
-               //Open connection
-               if (this.OpenConnection() == true)
-               {
                     //create mysql command
                     MySqlCommand cmd = new MySqlCommand();
                     //Assign the query using CommandText
@@ -135,77 +141,48 @@ namespace RSdbconnection
 
                     //Execute query
                     cmd.ExecuteNonQuery();
-           
-                    //close connection
-                    this.CloseConnection();
-               }
           }
           //Pre: Must pass a select query through this function with appropriate syntax based on mySQL
           //Post: Prints the records fetched on console & stores data into list of lists
           public List<List<string>> Select(string query)
           {
-               //Open connection
                List<List<string>> queryStorage = new List<List<string>>();
-               if (this.OpenConnection() == true)
+
+               //Create Command
+               MySqlCommand cmd = new MySqlCommand(query, connection);
+               //Create a data reader and Execute the command
+               MySqlDataReader dataReader = cmd.ExecuteReader();
+
+               //Count number of fields in retreived from the table & PRINT them on console
+               int fieldCount = dataReader.FieldCount;
+               for (int i = 0; i < fieldCount; i++)
                {
-                    //Create Command
-                    MySqlCommand cmd = new MySqlCommand(query, connection);
-                    //Create a data reader and Execute the command
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
-
-                    //Count number of fields in retreived from the table & PRINT them on console
-                    int fieldCount = dataReader.FieldCount;
-                    for (int i = 0; i < fieldCount; i++)
-                    {
-                         Console.Write(dataReader.GetName(i).PadRight(15));
-                    }
-                    Console.WriteLine();
-                    
-                    //Read records fetched from database & store them in list of lists.
-                    while (dataReader.Read())
-                    {
-                         List<string> temp = new List<string>();
-                         for(int i = 0; i < fieldCount; i++)
-                         {
-                              Console.Write(dataReader[i].ToString().PadRight(15));
-                              temp.Add(dataReader[i].ToString()); //Add all fields of query into inner list
-                         }
-                         queryStorage.Add(temp); //Store the record with fields(stored in temp) into outer list
-                         Console.WriteLine(); //End line -> new record
-                    }
-
-                    //close Data Reader
-                    dataReader.Close();
-
-                    //close Connection
-                    this.CloseConnection();
-                    return queryStorage; //Filled with query records
+                    Console.Write(dataReader.GetName(i).PadRight(15));
                }
-               else
+               Console.WriteLine();
+               
+               //Read records fetched from database & store them in list of lists.
+               while (dataReader.Read())
                {
-                    Console.WriteLine("Connection is not open!");
-                    return queryStorage; //Empty (most likely)
+                    List<string> temp = new List<string>();
+                    for(int i = 0; i < fieldCount; i++)
+                    {
+                         Console.Write(dataReader[i].ToString().PadRight(15));
+                         temp.Add(dataReader[i].ToString()); //Add all fields of query into inner list
+                    }
+                    queryStorage.Add(temp); //Store the record with fields(stored in temp) into outer list
+                    Console.WriteLine(); //End line -> new record
                }
+               dataReader.Close();
+               return queryStorage; //Filled with query records
           }
           public MySqlDataReader SelectDataReader(string query)
           {
-               //Create Command
-               if (this.OpenConnection() == true)
-               {
-                    MySqlCommand cmd = new MySqlCommand(query, connection);
-                    //Create a data reader and Execute the command
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
+               MySqlCommand cmd = new MySqlCommand(query, connection);
+               //Create a data reader and Execute the command
+               MySqlDataReader dataReader = cmd.ExecuteReader();
 
-                    return dataReader;
-               }
-               else
-               {
-                    query = "";
-                    MySqlCommand cmd = new MySqlCommand(query, connection);
-                    //Create a data reader and Execute the command
-                    MySqlDataReader dataReader = cmd.ExecuteReader();
-                    return dataReader;
-               }
+               return dataReader;
           }
      }
 
--- RSdbconnection/RSdbconnection/test.cs
+++ RSdbconnection/RSdbconnection/test.cs
@@ -22,11 +22,11 @@ namespace ConnectMySQL
         {
             Console.WriteLine("Getting Connection ...");
             DBConnect connection = new DBConnect();
-            List<List<string>> routesExecutedMETRIC = connection.Select("select route_id, train_id, computed_Route, route_type from RSdatabase.inprog_routes where time_completed != NULL; ");
-            List<List<string>> onTimeTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 0;");
-            double perOnTimeTrain = percentOnTime(ref onTimeTrainMETRIC, ref routesExecutedMETRIC);
-            Console.WriteLine(perOnTimeTrain);
-            List<List<string>> lateTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 1;");
+            //List<List<string>> routesExecutedMETRIC = connection.Select("select route_id, train_id, computed_Route, route_type from RSdatabase.inprog_routes where time_completed != NULL; ");
+            //List<List<string>> onTimeTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 0;");
+            //double perOnTimeTrain = percentOnTime(ref onTimeTrainMETRIC, ref routesExecutedMETRIC);
+            //Console.WriteLine(perOnTimeTrain);
+            //List<List<string>> lateTrainMETRIC = connection.Select("select distinct train_id from RSdatabase.inprog_routes where late = 1;");
             List<List<string>> mostUsedTrainMETRIC = connection.Select("select train_id, num_times_used from RSdatabase.train order by num_times_used desc; ");
             List<List<string>> mostUsedHubMETRIC = connection.Select("select hub_id, num_times_used from RSdatabase.hub order by num_times_used desc; ");
             List<List<string>> mostUsedStationMETRIC = connection.Select("select station_id, num_times_used from RSdatabase.station order by num_times_used desc; ");
